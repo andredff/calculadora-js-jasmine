c88ec7aec57d09862e85219b77e5eedc5a2f88a6
André Souza <andredff@gmail.com>
2020-04-06 20:15:50 -0300

    refactor

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,42 +1,61 @@
 const Calculadora = {
-  adicionar: (n1, n2) => {
-    if(isNaN(n1) || isNaN(n2)) {
-      return 0;
-    }
+  ADICAO: '+',
+  SUBTRACAO: '-',
+  DIVISAO: '/',
+  MULTIPLICACAO: '*',
 
-    n1 = parseFloat(n1);
-    n2 = parseFloat(n2);
-    return n1 + n2;
+
+  adicionar: (n1, n2) => {
+    return Calculadora.calcular(n1, n2, Calculadora.ADICAO);
   },
 
   subtrair: (n1, n2) => {
-    if(isNaN(n1) || isNaN(n2)) {
-      return 0;
-    }
-
-    return n1 - n2;
+    return Calculadora.calcular(n1, n2, Calculadora.SUBTRACAO);
   },
 
   dividir: (n1, n2) => {
     if (n2 === 0) { 
       return 'Erro';
     }
-
-    if(isNaN(n1) || isNaN(n2)) {
-      return 0;
-    }
-
-    return n1 / n2;
+    return Calculadora.calcular(n1, n2, Calculadora.DIVISAO);
   },
 
   multiplicar: (n1, n2) => {
+    return Calculadora.calcular(n1, n2, Calculadora.MULTIPLICACAO);
+  },
+
+  calcular: (n1, n2, operacao) => {
+    let resultado = 0;
+
+    n1 = parseFloat(n1);
+    n2 = parseFloat(n2);
+    
     if(isNaN(n1) || isNaN(n2)) {
       return 0;
     }
-    
-    return n1 * n2;
+
+    switch (operacao) {
+      case Calculadora.ADICAO:
+        resultado = n1 + n2;
+        break;
+      case Calculadora.SUBTRACAO:
+        resultado = n1 - n2;
+        break;
+      case Calculadora.DIVISAO:
+        resultado = n1 / n2;
+        break; 
+      case Calculadora.MULTIPLICACAO:
+        resultado = n1 * n2;
+        break;   
+      default:
+        resultado = 0;
+        break;
+    }
+    return resultado;
+
   }
-}
+
+};
 
 if (typeof module !== 'undefined') {
   module.exports = Calculadora;
